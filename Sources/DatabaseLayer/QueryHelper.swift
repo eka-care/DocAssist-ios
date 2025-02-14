56325c6c04db7dd5658e714d037bd3d53a226755
aryaIOS <arya.vashisht@eka.care>
2025-02-14 16:17:12 +0530

    Core data changes

--- Sources/DatabaseLayer/QueryHelper.swift
+++ Sources/DatabaseLayer/QueryHelper.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftData
+import CoreData
 import Foundation
 
 // Sessions
@@ -33,6 +34,43 @@ extension QueryHelper {
     return fetchDescriptor
     
   }
-  
-
 }
+
+// MARK: - Core data
+
+extension QueryHelper {
+  /// Fetch message with messageID and sessionID
+  static func fetchMessage(
+    messageID: Int,
+    sessionID: String,
+    context: NSManagedObjectContext
+  ) -> NSFetchRequest<ChatData>? {
+    let fetchRequest: NSFetchRequest<ChatData> = ChatData.fetchRequest()
+    fetchRequest.predicate = NSPredicate(format: "toSessionData.sessionId == %@ AND messageID == %d", sessionID, messageID)
+    fetchRequest.sortDescriptors = [NSSortDescriptor(key: "messageID", ascending: false)]
+    fetchRequest.fetchLimit = 1
+    return fetchRequest
+  }
+  
+  static func fetchSession(sessionID: String) -> NSFetchRequest<SessionData> {
+    let fetchRequest: NSFetchRequest<SessionData> = SessionData.fetchRequest()
+    fetchRequest.predicate = NSPredicate(format: "sessionId == %@", sessionID)
+    fetchRequest.fetchLimit = 1
+    return fetchRequest
+  }
+  
+//  static func fetchMessage(
+//    messageID: Int,
+//    sessionID: UUID,
+//    context: NSManagedObjectContext
+//  ) throws -> ChatData? {
+//    let fetchRequest: NSFetchRequest<ChatData> = ChatData.fetchRequest()
+//    fetchRequest.predicate = NSPredicate(format: "toSessionData.sessionID == %@ AND messageID == %d", sessionID as CVarArg, messageID)
+//    fetchRequest.sortDescriptors = [NSSortDescriptor(key: "messageID", ascending: false)]
+//    fetchRequest.fetchLimit = 1
+//    
+//    let results = try context.fetch(fetchRequest)
+//    return results.first
+//  }
+}
+
