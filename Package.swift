e2ff49634b58cf0c92cf48f639147a0a492e9925
aryaIOS <arya.vashisht@eka.care>
2025-03-26 16:01:49 +0530

    Branch change in eka pdf maker

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
     // VoiceToRx
     .package(url: "https://github.com/eka-care/EkaVoiceToRx.git", from: "1.1.1"),
     // EkaPDFMaker
-    .package(url: "https://github.com/aryaIOS/EkaPDFMaker.git", branch: "fix/clipping-of-view-in-swiftui")
+    .package(url: "https://github.com/aryaIOS/EkaPDFMaker.git", branch: "main")
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
