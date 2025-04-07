b666e8960d3e03e800dad35d91c07bcd39028fdf
BRUNDAB19B <brunda.b@eka.care>
2025-04-07 10:03:06 +0530

    Upgrade tag

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
     // Mixpanel
     .package(url: "https://github.com/mixpanel/mixpanel-swift", from: "3.2.5"),
     // VoiceToRx
-    .package(url: "https://github.com/eka-care/EkaVoiceToRx.git", from: "1.1.1"),
+    .package(url: "https://github.com/eka-care/EkaVoiceToRx.git", from: "1.1.2"),
     // EkaPDFMaker
     .package(url: "https://github.com/aryaIOS/EkaPDFMaker.git", branch: "main")
   ],
