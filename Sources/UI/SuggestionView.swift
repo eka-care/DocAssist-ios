22cd29229e2d9aa3f1c6372a536b034712c93760
BRUNDAB19B <brunda.b@eka.care>
2025-04-17 11:23:09 +0530

    Ui updates

--- Sources/UI/SuggestionView.swift
+++ Sources/UI/SuggestionView.swift
@@ -11,29 +11,39 @@ struct SuggestionView: View {
   
   var suggestionText: [String]?
   var viewModel: ChatViewModel
+  var lastMessageId: Int?
   
   private var constantSuggestionString = "Suggested questions you can ask me-"
   
-  init(suggestionText: [String]?, viewModel: ChatViewModel) {
+  init(suggestionText: [String]?, viewModel: ChatViewModel, lastMessageId: Int? = nil) {
     self.suggestionText = suggestionText
     self.viewModel = viewModel
+    self.lastMessageId = lastMessageId
   }
   
   var body: some View {
     VStack {
-      Text(constantSuggestionString)
-        .font(.custom("Lato-Regular", size: 12))
-        .foregroundColor(Color(red: 0.46, green: 0.46, blue: 0.46))
-        .frame(width: 306, height: 16, alignment: .topLeading)
+      
+      HStack {
+        BotAvatarImage()
+          .alignmentGuide(.top) { d in d[.top] }
+        Text(constantSuggestionString)
+          .font(.custom("Lato-Regular", size: 12))
+          .foregroundColor(Color(red: 0.46, green: 0.46, blue: 0.46))
+          .frame(width: 306, height: 16, alignment: .topLeading)
+      }
+      
       ForEach(suggestionText ?? ["Show HbA1c trend of this patient", "List Lab investigations prescribed in past 1 year"], id: \.self) { suggestionText in
         Button(action: {
+          guard let lastMessageId else { return }
           Task {
-            await viewModel.sendMessage(newMessage: suggestionText, imageUrls: nil, vaultFiles: nil, sessionId: viewModel.vmssid, lastMesssageId: nil)
+            await viewModel.sendMessage(newMessage: suggestionText, imageUrls: nil, vaultFiles: nil, sessionId: viewModel.vmssid, lastMesssageId: lastMessageId)
           }
         }) {
           HStack {
             Text(suggestionText)
               .foregroundColor(Color.primaryprimary)
+              .font(.custom("Lato-Regular", size: 14))
               .multilineTextAlignment(.leading)
               .padding(.init(top: 8, leading: 12, bottom: 8, trailing: 12))
               .frame(maxWidth: .infinity, alignment: .leading)
@@ -41,6 +51,7 @@ struct SuggestionView: View {
               .clipShape(RoundedRectangle(cornerRadius: 12))
             Spacer()
           }
+          .padding(.leading, 16)
         }
       }
     }
