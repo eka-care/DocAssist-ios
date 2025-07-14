5b2474d7b8021372c6adf1cb9c6f2a8f4cbb7d39
aryaIOS <arya.vashisht@eka.care>
2025-07-14 11:41:01 +0530

    package update

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     // Mixpanel
     .package(url: "https://github.com/mixpanel/mixpanel-swift", from: "3.2.5"),
     // VoiceToRx
-    .package(url: "https://github.com/eka-care/EkaVoiceToRx.git", branch: "fix/voice-to-rx-session-limit"),
+    .package(url: "https://github.com/eka-care/EkaVoiceToRx.git", from: "1.2.1"),
     // EkaPDFMaker
     .package(url: "https://github.com/aryaIOS/EkaPDFMaker.git", branch: "main")
   ],
