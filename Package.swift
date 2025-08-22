8114023980a0783ce0a22b7d25989e7bcc0cc546
ShekharEkaCare <shekhar.gupta@eka.care>
2025-08-22 20:42:44 +0530

    Update Package

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     // FireBase package dependency
     .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.0.1"),
     // Medical Record
-    .package(url: "git@github.com:eka-care/EkaMedicalRecordsUI.git", from: "1.1.0"),
+    .package(url: "git@github.com:eka-care/EkaMedicalRecordsUI.git", from: "1.1.1"),
     // Mixpanel
     .package(url: "https://github.com/mixpanel/mixpanel-swift", from: "3.2.5"),
     // VoiceToRx
