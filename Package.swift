7e6a84303e17dd130f8b93c5fb13abbc9b209510
shekhar gupta <shekhar.gupta@eka.care>
2025-11-24 19:46:36 +0530

    Package fix

--- Package.swift
+++ Package.swift
@@ -9,42 +9,27 @@ let package = Package(
     .iOS(.v17)
   ],
   products: [
-    // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
       name: "ChatBotAiPackage",
       targets: ["ChatBotAiPackage"]),
   ],
   dependencies: [
-    // MarkdownUI package dependency
     .package(url: "https://github.com/gonzalezreal/swift-markdown-ui", from: "2.4.1"),
-    // FireBase package dependency
     .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.0.1"),
-    // Medical Record
-    .package(url: "https://github.com/eka-care/EkaMedicalRecordsUI.git",  from: "1.3.5"),
-    // Mixpanel
+    .package(url: "git@github.com:eka-care/EkaMedicalRecordsUI.git",  from: "1.3.5"),
     .package(url: "https://github.com/mixpanel/mixpanel-swift", from: "3.2.5"),
-    // VoiceToRx
-    .package(url: "https://github.com/eka-care/EkaVoiceToRx.git", branch: "main"),
-    // EkaPDFMaker
-    .package(url: "https://github.com/eka-care/EkaPDFMaker.git", branch: "main")
+    .package(url: "git@github.com:eka-care/EkaVoiceToRx.git", branch: "main"),
+    .package(url: "git@github.com:eka-care/EkaPDFMaker.git", branch: "main")
   ],
   targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
     .target(
       name: "ChatBotAiPackage",
       dependencies: [
-        // MarkdownUI
         .product(name: "MarkdownUI", package: "swift-markdown-ui"),
-        // FireBase
         .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
-        // Medical Record
         .product(name: "EkaMedicalRecordsUI", package: "EkaMedicalRecordsUI"),
-        // Mixpanel
         .product(name: "Mixpanel", package: "mixpanel-swift"),
-        // VoiceToRx
         .product(name: "EkaVoiceToRx", package: "EkaVoiceToRx"),
-        // PdfRender
         .product(name: "EkaPDFMaker", package: "EkaPDFMaker")
       ],
       resources: [
