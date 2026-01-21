0ea7cb5dd69bb41cdc3e4db6d50d117d7bd1e199
shekhar <shekhar.gupta@eka.care>
2026-01-21 19:42:14 +0530

    added voiceToRx repo

--- Package.swift
+++ Package.swift
@@ -23,6 +23,8 @@ let package = Package(
     .package(url: "git@github.com:eka-care/EkaMedicalRecordsUI.git",  branch: "1.4.0"),
     // Mixpanel
     .package(url: "https://github.com/mixpanel/mixpanel-swift", from: "3.2.5"),
+    // VoiceToRx
+    .package(url: "https://github.com/eka-care/EkaVoiceToRx.git", branch: "main"),
     // EkaPDFMaker
     .package(url: "git@github.com:eka-care/EkaPDFMaker.git", branch: "main")
   ],
